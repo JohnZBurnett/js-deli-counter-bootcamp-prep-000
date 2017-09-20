4667dafe9fef31fa56a9925893009a1f6866aa88
John Burnett <tuninator@gmail.com>
2017-09-20 22:32:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
 
-  return `Welcome, ${name}. You are number ${(katzDeliLine.length + 1)} in line.` 
+  return `Welcome, ${name}. You are number ${(katzDeliLine.length + 1)} in line.`
 
 
 }
